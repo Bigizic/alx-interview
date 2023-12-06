546eb3a982e3b4f3835d3a31287683b526c2c63b
Bigizic <olalekanisaac75@gmail.com>
2023-12-06 01:03:56 +0000

    solved task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Implementation of the lockboxes function
+"""
+
+
+def canUnlockAll(boxes):
+    """A function that determines if all the boxes in a list of lists
+    can be accessed from box[0] """
+    boxes_length = len(boxes) - 1
+    myList = []
+    newList = []
+
+    for keys in range(boxes_length):
+        box_length = len(boxes[keys])
+        myList.append(box_length)
+
+    for items in myList:
+        if items > 0:
+            newList.append(True)
+        else:
+            newList.append(False)
+    if False in newList:
+        return False
+    return True
