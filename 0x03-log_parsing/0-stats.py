5e4db5910096873ff83f6dafdfa7df812b09faae
Bigizic <olalekanisaac75@gmail.com>
2023-12-21 00:45:46 +0000

    Update stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,24 +4,35 @@
 
 import sys
 
+output = sys.stdin
 
-def log_dict(data, ctrlC=None):
-    """parse the striped and splited list for the status_code and file_size
-    @param (data): <list>
 
-    Return: void
+def log_parsing():
+    """implementation
     """
     big_list = []
     total_items = 0
     status_count = {}
 
+    try:
+        for line in output:
+            data = line.strip().split()
+            log_helper(data, big_list, total_items, status_count)
+
+    except KeyboardInterrupt:
+        return
+
+
+def log_helper(data, big_list, total_items, status_count):
+    """Helper function for log_parsing
+    """
     status_code = int(data[-2])
     file_size = int(data[-1])
 
     if isinstance(status_code, int) and isinstance(file_size, int):
         my_dict = {'status_code': status_code, 'file_size': file_size}
         big_list.append(my_dict)
-        print(f'{status_code}: {file_size}')
+        # print(f'{status_code}: {file_size}')
 
         if len(big_list) == 10:
             for items in big_list:
@@ -30,28 +41,15 @@ def log_dict(data, ctrlC=None):
                 status_count[code] = status_count.get(code, 0) + 1
             print(f'File size: {total_items}')
 
-            for key_code, val_count in sorted(status_count.items()):
-                print(f'{key_code}: {val_count}')
-            big_list.clear()
-            # status_code.clear()
-        if ctrlC:
-            sys.exit(1)
-
-
-def log_parsing():
-    """Implementation
-    """
-    for line in sys.stdin:
-        try:
-            data = line.strip().split()
-            log_dict(data)
-
             """for items in big_list:
             print('{}: {}'.format(items.get('status_code'),
             items.get('file_size')))"""
-        except KeyboardInterrupt:
-            data = line.strip().split()
-            log_dict(data, True)
+
+            for key_code, val_count in sorted(status_count.items()):
+                print(f'{key_code}: {val_count}')
+            print(big_list)
+            big_list.clear()
+            # status_code.clear()
 
 
 if __name__ == '__main__':
