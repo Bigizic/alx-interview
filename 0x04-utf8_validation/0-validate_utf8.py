35d73e9b855e6e59d64963740f0e0338bfa6744b
Bigizic <olalekanisaac75@gmail.com>
2024-01-04 11:23:16 +0000

    update solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,22 +7,24 @@ def validUTF8(data):
     """@param (data): <list>
     Return: True if data is valid UTF-8 encodable otherwise false
     """
-    count = 0
-    for item in data:
-        if count == 0:
-            if (item >> 7) == 0b0:
-                continue
-            elif (item >> 5) == 0b110:
-                count = 1
-            elif (item >> 4) == 0b1110:
-                count = 2
-            elif (item >> 3) == 0b11110:
-                count = 3
+    if data:
+        count = 0
+        for item in data:
+            if count == 0:
+                if (item >> 7) == 0b0:
+                    continue
+                elif (item >> 5) == 0b110:
+                    count = 1
+                elif (item >> 4) == 0b1110:
+                    count = 2
+                elif (item >> 3) == 0b11110:
+                    count = 3
+                else:
+                    return False
             else:
-                return False
-        else:
-            if (item >> 6) != 0b10:
-                return False
-            count -= 1
+                if (item >> 6) != 0b10:
+                    return False
+                count -= 1
 
-    return count == 0
+        return count == 0
+    return False
