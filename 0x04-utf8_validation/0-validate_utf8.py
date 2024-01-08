0de3c5b9c28f5ad2964f64f201cde5cfd7ee2838
Bigizic <olalekanisaac75@gmail.com>
2024-01-08 00:12:41 +0000

    Update validate_utf.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,12 +12,14 @@ def validUTF8(data):
         if count == 0:
             if (item >> 7) == 0b0:
                 continue
-            elif (item >> 5) == 0b110:
+            elif (item >> 6) == 0b10:
                 count = 1
-            elif (item >> 4) == 0b1110:
+            elif (item >> 5) == 0b110:
                 count = 2
-            elif (item >> 3) == 0b11110:
+            elif (item >> 4) == 0b1110:
                 count = 3
+            elif (item >> 3) == 0b11110:
+                count = 4
             else:
                 return True
         else:
