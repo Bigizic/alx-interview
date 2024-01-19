2d9e44c39ad731f81adfba6b7d4b1f6d2ee97aea
Bigizic <olalekanisaac75@gmail.com>
2024-01-19 17:22:03 +0000

    Update starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
 
 const starWar = (url) => {
-  const number = process.argv[2];
   const request = require('request');
   request.get(url, (error, response, body) => {
     if (error) { console.error(error); return; }
     const data = JSON.parse(body);
-    for (const items of data.results) {
-      if (parseInt(number) === items.episode_id) {
-        async function letsWaitForNames () {
-          for (const link of items.characters) {
-            await new Promise((resolve, reject) => {
-              request.get(link, (err, resp, body) => {
-                if (err) { reject(err); }
-                const name = JSON.parse(body);
-                console.log(name.name);
-                resolve();
-              });
-            });
-          }
-        }
-        letsWaitForNames();
+    const whatINeed = 'characters';
+    const charsLink = data[`${whatINeed}`];
+    async function letsWaitForNames () {
+      for (const items of charsLink) {
+        await new Promise((resolve, reject) => {
+          request(items, { json: true }, (err, resp, body) => {
+            if (err) { reject(err); return; }
+            console.log(body.name);
+            resolve();
+          });
+        });
       }
     }
+    letsWaitForNames();
   });
 };
-starWar('https://swapi-api.alx-tools.com/api/films/');
+const number = process.argv[2];
+starWar(`https://swapi-api.alx-tools.com/api/films/${number}`);
 // starWar('https://ezyurl.xyz');
