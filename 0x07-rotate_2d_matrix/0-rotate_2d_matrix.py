916db0287c0e3fe795eed7e635e8428b8197a276
Bigizic <olalekanisaac75@gmail.com>
2024-01-26 02:04:16 +0000

    add updates

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,7 +4,20 @@
 
 
 def rotate_2d_matrix(matrix):
-    """Implementation
+    """function attempts to rotate a n by n nested list to 90 deg
+
+    for each set in matrix the index of each sets are taken for each rows
+    and appends the integers in sm, then creates a nested list of each
+    element in sm to align it in a n by n nested list cuz sm contains each
+    element in matrix in a long list and not nested
+
+    e.g of sm: [1, 4, 7, 2, 5, 8, 3, 6, 9]
+
+    e.g of s: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
+    e.g of temp: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+    matrix[0 to 2 + 1] = temp
     """
     m_len = len(matrix) - 1
     sm = []
