975b4637469de7d7741a47a13cb37bb99ca8496c
Bigizic <olalekanisaac75@gmail.com>
2024-02-07 02:24:32 +0000

    add changes

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,27 +11,17 @@ def island_perimeter(grid):
     """
     n_row = len(grid)
     n_col = len(grid[0])
-    col_count = 0
-    row_count = 0
+    perimeter = 0
 
-    # maximum none zero
-    # check for the row that has more than none zero character
-    # creates a new list that forms from each column in grid
-    max_nz = max(sum(grid[r][c] for r in range(n_row)) for c in range(n_col))
-    new_list = [[0 for _ in range(max_nz)] for _ in range(n_col)]
-    for col in range(n_col):
-        non_zero_count = sum(grid[row][col] for row in range(n_row))
-        for i in range(non_zero_count):
-            new_list[col][i] = 1
-
-    for rows in grid:
-        if isinstance(rows, list):
-            if 1 in rows:
-                row_count += 1
-
-    for rows in new_list:
-        if isinstance(rows, list):
-            if 1 in rows:
-                col_count += 1
-    parameter = 2 * (row_count + col_count)
-    return parameter
+    for row in range(n_row):
+        for col in range(n_col):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == n_row - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == n_col - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
